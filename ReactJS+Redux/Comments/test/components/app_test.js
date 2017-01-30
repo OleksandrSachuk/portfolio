a8433f1223fb6c6ad36cb861e255f48ddb6205ec
Oleksandr Sachuk <ovsachuk@gmail.com>
2017-01-30 17:59:22 +0200

    Comments

--- ReactJS+Redux/Comments/test/components/app_test.js
+++ ReactJS+Redux/Comments/test/components/app_test.js
@@ -3,12 +3,16 @@ import App from '../../src/components/app';
 
 describe('App' , () => {
   let component;
-
   beforeEach(() => {
     component = renderComponent(App);
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
+
+  it('shows a comments list box', () => {
+    expect(component.find('.comment-list')).to.exist;
+  });
+
 });
